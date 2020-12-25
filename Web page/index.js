49050a92a7780626f64bb88ef790ec7513403879
Veencoder <63860637+Veencoder@users.noreply.github.com>
2020-12-25 17:38:32 +0100

    initial

---
+++ Web page/index.js
@@ -0,0 +1,9 @@
+
+$("#about ").bind("mouseover mouseout", function() {
+  var shadow= $("#about");
+   shadow.toggleClass("mouseover");
+});
+$("#work ").bind("mouseover mouseout", function() {
+  var shadow= $("#work");
+   shadow.toggleClass("mouseover");
+});
